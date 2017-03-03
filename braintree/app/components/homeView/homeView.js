6ff480746d4c47ea02b87d8b9def72b7a3306aca
Kelson Schmitt <ks@doohsmedia.com>
2017-03-03 04:03:00 +0000

    Changes made by Kelson Schmitt (ks@doohsmedia.com). Commit

--- braintree/app/components/homeView/homeView.js
+++ braintree/app/components/homeView/homeView.js
@@ -5,63 +5,82 @@ var isInit = true,
     // additional requires
     viewModel = require('./homeView-view-model');
 
-var braintree = require('braintree');
+// var braintree = require('braintree');
 
-var gateway = braintree.connect({
-    environment: braintree.Environment.Sandbox,
-    merchantId: 'your_merchant_id',
-    publicKey: 'your_public_key',
-    privateKey: 'your_private_key'
-});
+// var gateway = braintree.connect({
+//     environment: braintree.Environment.Sandbox,
+//     merchantId: 'your_merchant_id',
+//     publicKey: 'your_public_key',
+//     privateKey: 'your_private_key'
+// });
 
-gateway.transaction.sale({
-    amount: '5.00',
-    paymentMethodNonce: 'nonce-from-the-client',
-    options: {
-        submitForSettlement: true
-    }
-}, function (err, result) {
-    if (err) {
-        alert(err);
-        return;
-    }
+// gateway.transaction.sale({
+//     amount: '5.00',
+//     paymentMethodNonce: 'nonce-from-the-client',
+//     options: {
+//         submitForSettlement: true
+//     }
+// }, function (err, result) {
+//     if (err) {
+//         alert(err);
+//         return;
+//     }
 
-    if (result.success) {
-        alert('Transaction ID: ' + result.transaction.id);
-    } else {
-        alert(result.message);
-    }
-}); var gateway = braintree.connect({
-    environment: braintree.Environment.Sandbox,
-    merchantId: 'your_merchant_id',
-    publicKey: 'your_public_key',
-    privateKey: 'your_private_key'
-});
+//     if (result.success) {
+//         alert('Transaction ID: ' + result.transaction.id);
+//     } else {
+//         alert(result.message);
+//     }
+// }); var gateway = braintree.connect({
+//     environment: braintree.Environment.Sandbox,
+//     merchantId: 'your_merchant_id',
+//     publicKey: 'your_public_key',
+//     privateKey: 'your_private_key'
+// });
 
-gateway.transaction.sale({
-    amount: '5.00',
-    paymentMethodNonce: 'nonce-from-the-client',
-    options: {
-        submitForSettlement: true
-    }
-}, function (err, result) {
-    if (err) {
-        alert(err);
-        return;
-    }
+// gateway.transaction.sale({
+//     amount: '5.00',
+//     paymentMethodNonce: 'nonce-from-the-client',
+//     options: {
+//         submitForSettlement: true
+//     }
+// }, function (err, result) {
+//     if (err) {
+//         alert(err);
+//         return;
+//     }
 
-    if (result.success) {
-        alert('Transaction ID: ' + result.transaction.id);
-    } else {
-        alert(result.message);
-    }
-});
+//     if (result.success) {
+//         alert('Transaction ID: ' + result.transaction.id);
+//     } else {
+//         alert(result.message);
+//     }
+// });
 
 // additional functions
+
+var app = require("application");
+var observable = require("data/observable");
+
 function pageLoaded(args) {
     var page = args.object;
 
     helpers.platformInit(page);
+
+    //viewModel.set("htmlString", '<iframe width="100%" height="540" src="components/homeView/video/index.html" frameborder="0"></iframe>');
+
+    var glyphs = new Array();    
+    var charCode = 0xF102;
+    for (; charCode <= 0xF113; charCode++) {
+        var glyph = new observable.Observable();
+
+        glyph.set("icon", String.fromCharCode(charCode));
+        glyph.set("code", charCode.toString(16));
+        glyphs.push(glyph);
+    }
+    viewModel.set("glyphs", glyphs);
+
+
     page.bindingContext = viewModel;
     // additional pageLoaded
 
@@ -69,6 +88,16 @@ function pageLoaded(args) {
         isInit = false;
 
         // additional pageInit
+    }
+
+    var webview = page.getViewById("wv");
+    if (app.android) {
+        var wvSettings = webview.android.getSettings();
+        wvSettings.setJavaScriptEnabled(true);
+        wvSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        wvSettings.setDomStorageEnabled(true);
+
+        viewModel.set("htmlString", '<iframe width="100%" height="540" src="components/homeView/video/index.html" frameborder="0"></iframe>');
     }
 }
 
